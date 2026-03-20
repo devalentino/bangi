651e3108827577e3dc76aad2becb01807d758baf
Valentin Saulyak <valentin.saulyak@gmail.com>
2026-03-20 15:32:13 +0200

    Facebook PACS Access URL expiration alert rework (#60)

--- tests/integration/facebook_pacs/test_alerts.py
+++ tests/integration/facebook_pacs/test_alerts.py
@@ -1,20 +1,45 @@
-from datetime import datetime, timezone
-
-
 class TestAlerts:
-    def test_get_alerts__expired_business_portfolio_access_url(
+    def test_get_alerts__business_portfolio_without_access_urls(self, client, authorization, business_portfolio):
+        response = client.get('/api/v2/alerts', headers={'Authorization': authorization})
+
+        assert response.status_code == 200, response.text
+        assert response.json == {
+            'content': [
+                {
+                    'code': 'facebook_pacs_business_portfolio_access_url_missing',
+                    'message': f'Business portfolio "{business_portfolio["name"]}" does not have access URLs',
+                    'severity': 'warning',
+                    'source': 'src.facebook_pacs.services',
+                    'payload': {
+                        'businessPortfolioId': business_portfolio['id'],
+                        'businessPortfolioName': business_portfolio['name'],
+                    },
+                }
+            ]
+        }
+
+    def test_get_alerts__business_portfolio_with_only_expired_access_urls(
         self, client, authorization, business_portfolio, timestamp, write_to_db
     ):
-        access_url = write_to_db(
+        write_to_db(
             'facebook_pacs_business_portfolio_access_url',
             {
                 'business_portfolio_id': business_portfolio['id'],
-                'url': 'http://localhost',
+                'url': 'http://localhost/expired-1',
                 'email': 'owner@example.com',
-                'expires_at': timestamp,
+                'expires_at': timestamp - 1,
             },
         )
-        expires_at_iso = datetime.utcfromtimestamp(timestamp).replace(tzinfo=timezone.utc).isoformat()
+
+        write_to_db(
+            'facebook_pacs_business_portfolio_access_url',
+            {
+                'business_portfolio_id': business_portfolio['id'],
+                'url': 'http://localhost/expired-2',
+                'email': 'owner@example.com',
+                'expires_at': timestamp - 1,
+            },
+        )
 
         response = client.get('/api/v2/alerts', headers={'Authorization': authorization})
 
@@ -23,37 +48,48 @@ class TestAlerts:
             'content': [
                 {
                     'code': 'facebook_pacs_business_portfolio_access_url_expired',
-                    'message': (
-                        f'Business portfolio access URL expired for "{business_portfolio["name"]}" on {expires_at_iso}.'
-                    ),
+                    'message': f'Business portfolio access URLs "{business_portfolio["name"]}" expired',
                     'severity': 'warning',
                     'source': 'src.facebook_pacs.services',
                     'payload': {
-                        'accessUrlId': access_url['id'],
                         'businessPortfolioId': business_portfolio['id'],
                         'businessPortfolioName': business_portfolio['name'],
-                        'url': access_url['url'],
-                        'email': access_url['email'],
-                        'expiresAt': timestamp,
                     },
                 }
             ]
         }
 
-    def test_get_alerts__business_portfolio_access_url_expires_soon(
+    def test_get_alerts__business_portfolio_with_one_expiring_soon_access_url(
         self, client, authorization, business_portfolio, timestamp, write_to_db
     ):
-        access_url = write_to_db(
+        write_to_db(
             'facebook_pacs_business_portfolio_access_url',
             {
                 'business_portfolio_id': business_portfolio['id'],
-                'url': 'http://localhost',
+                'url': 'http://localhost/expired-1',
                 'email': 'owner@example.com',
-                'expires_at': timestamp + 5 * 24 * 60 * 60,
+                'expires_at': timestamp - 1,
             },
         )
-        expires_at_iso = (
-            datetime.utcfromtimestamp(timestamp + 5 * 24 * 60 * 60).replace(tzinfo=timezone.utc).isoformat()
+
+        write_to_db(
+            'facebook_pacs_business_portfolio_access_url',
+            {
+                'business_portfolio_id': business_portfolio['id'],
+                'url': 'http://localhost/expired-2',
+                'email': 'owner@example.com',
+                'expires_at': timestamp - 1,
+            },
+        )
+
+        write_to_db(
+            'facebook_pacs_business_portfolio_access_url',
+            {
+                'business_portfolio_id': business_portfolio['id'],
+                'url': 'http://localhost/expired-3',
+                'email': 'owner@example.com',
+                'expires_at': timestamp + 24 * 60 * 60,
+            },
         )
 
         response = client.get('/api/v2/alerts', headers={'Authorization': authorization})
@@ -63,21 +99,99 @@ class TestAlerts:
             'content': [
                 {
                     'code': 'facebook_pacs_business_portfolio_access_url_expiring_soon',
-                    'message': (
-                        f'Business portfolio access URL for "{business_portfolio["name"]}" '
-                        f'expires on {expires_at_iso}.'
-                    ),
+                    'message': f'Business portfolio access URL "{business_portfolio["name"]}" expires soon',
                     'severity': 'info',
                     'source': 'src.facebook_pacs.services',
                     'payload': {
-                        'accessUrlId': access_url['id'],
                         'businessPortfolioId': business_portfolio['id'],
                         'businessPortfolioName': business_portfolio['name'],
-                        'url': access_url['url'],
-                        'email': access_url['email'],
-                        'expiresAt': timestamp + 5 * 24 * 60 * 60,
-                        'daysUntilExpiration': 5,
                     },
                 }
             ]
         }
+
+    def test_get_alerts__business_portfolio_with_one_in_date_access_url(
+        self, client, authorization, business_portfolio, timestamp, write_to_db
+    ):
+        write_to_db(
+            'facebook_pacs_business_portfolio_access_url',
+            {
+                'business_portfolio_id': business_portfolio['id'],
+                'url': 'http://localhost/expired-1',
+                'email': 'owner@example.com',
+                'expires_at': timestamp - 1,
+            },
+        )
+
+        write_to_db(
+            'facebook_pacs_business_portfolio_access_url',
+            {
+                'business_portfolio_id': business_portfolio['id'],
+                'url': 'http://localhost/expired-2',
+                'email': 'owner@example.com',
+                'expires_at': timestamp - 1,
+            },
+        )
+
+        write_to_db(
+            'facebook_pacs_business_portfolio_access_url',
+            {
+                'business_portfolio_id': business_portfolio['id'],
+                'url': 'http://localhost/expired-3',
+                'email': 'owner@example.com',
+                'expires_at': timestamp + 6 * 24 * 60 * 60,
+            },
+        )
+
+        response = client.get('/api/v2/alerts', headers={'Authorization': authorization})
+
+        assert response.status_code == 200, response.text
+        assert response.json == {'content': []}
+
+    def test_get_alerts__business_portfolio_with_expiring_soon_and_in_date_access_urls(
+        self, client, authorization, business_portfolio, timestamp, write_to_db
+    ):
+        write_to_db(
+            'facebook_pacs_business_portfolio_access_url',
+            {
+                'business_portfolio_id': business_portfolio['id'],
+                'url': 'http://localhost/expired-1',
+                'email': 'owner@example.com',
+                'expires_at': timestamp - 1,
+            },
+        )
+
+        write_to_db(
+            'facebook_pacs_business_portfolio_access_url',
+            {
+                'business_portfolio_id': business_portfolio['id'],
+                'url': 'http://localhost/expired-2',
+                'email': 'owner@example.com',
+                'expires_at': timestamp - 1,
+            },
+        )
+
+        write_to_db(
+            'facebook_pacs_business_portfolio_access_url',
+            {
+                'business_portfolio_id': business_portfolio['id'],
+                'url': 'http://localhost/expired-3',
+                'email': 'owner@example.com',
+                'expires_at': timestamp + 24 * 60 * 60,
+            },
+        )
+
+        write_to_db(
+            'facebook_pacs_business_portfolio_access_url',
+            {
+                'business_portfolio_id': business_portfolio['id'],
+                'url': 'http://localhost/expired-4',
+                'email': 'owner@example.com',
+                'expires_at': timestamp + 6 * 24 * 60 * 60,
+            },
+        )
+
+        response = client.get('/api/v2/alerts', headers={'Authorization': authorization})
+
+        assert response.status_code == 200, response.text
+        assert response.json == {'content': []}
