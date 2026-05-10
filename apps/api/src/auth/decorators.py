08f00a1ec0ce92b43856e940f53cce5ba60d533d
devalentino <valentin.saulyak@gmail.com>
2026-05-10 11:59:39 +0300

    KAN-16: fix: fix host disk stats ingestion script

--- apps/api/src/auth/decorators.py
+++ apps/api/src/auth/decorators.py
@@ -1,13 +1,14 @@
 from flask_httpauth import HTTPBasicAuth
 
 from src.auth.services import AuthenticationService
-from src.container import container
 
 auth = HTTPBasicAuth()
 
 
 @auth.verify_password
 def verify_password(username, password) -> None:
+    from src.container import container
+
     authentication_service = container.get(AuthenticationService)
     if authentication_service.authenticate(username.strip(), password.strip()):
         return username
