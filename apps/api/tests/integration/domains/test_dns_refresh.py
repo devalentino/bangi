21589f3a4a71184f8ef3952776e07dfbf90da5bd
devalentino <valentin.saulyak@gmail.com>
2026-05-12 15:26:49 +0300

    minor code improvements

--- apps/api/tests/integration/domains/test_dns_refresh.py
+++ apps/api/tests/integration/domains/test_dns_refresh.py
@@ -5,9 +5,9 @@ import pytest
 
 
 @pytest.fixture
-def dns_resolver_mock():
-    with mock.patch("src.domains.services.dns.resolver") as resolver:
-        resolver.resolve.return_value = [mock.Mock(address='203.0.113.10')]
+def dns_resolver_mock(public_ip):
+    with mock.patch('src.domains.services.dns.resolver') as resolver:
+        resolver.resolve.return_value = [mock.Mock(address=public_ip)]
         yield resolver
 
 
