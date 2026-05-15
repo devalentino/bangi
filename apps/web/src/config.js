8c3a35e9f5f5ff0eca71fe9c51b70f805c0a1574
devalentino <valentin.saulyak@gmail.com>
2026-05-15 22:20:36 +0300

    bump version

--- apps/web/src/config.js
+++ apps/web/src/config.js
@@ -4,7 +4,7 @@ if (typeof window !== "undefined" && window.APP_CONFIG) {
   runtimeConfig = window.APP_CONFIG;
 }
 
-var backendApiBaseUrl = "http://138.68.85.45/api/v2";
+var backendApiBaseUrl = "/api/v2";
 var debugPersistentAuth = runtimeConfig.DEBUG_PERSIST_AUTH === true
   || runtimeConfig.DEBUG_PERSIST_AUTH === "true";
 
